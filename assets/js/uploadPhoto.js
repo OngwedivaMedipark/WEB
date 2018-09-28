5e3bfbec8f9a639ffb9fa6ea0e91dd0461266318
Vuyiswa <vuyiswa@mgibagroup.com>
2018-09-28 13:13:38 +0200

    finalazing picture

--- assets/js/uploadPhoto.js
+++ assets/js/uploadPhoto.js
@@ -1,18 +1,34 @@
-/*function displayUpload(){
-var x = document.getElementById("uploadPhoto");
-    if(x.style.display ==="none"){
-         x.style.display = "block";
-    } else {
-    x.style.display ="none";
-    }
+function hideSnap(){
+var x = document.getElementById("snapCanvas").style.display="block";
+var y = document.getElementById("takePhoto").style.display="block";
+var z = document.getElementById("uploadPhoto");
+if(x.display ==="block")
+  {
+    z.style.display = "none";
+  }
+    console.log("snap is removed");
 }
-function displayPicture(){
+
+function showSnap(){
+  var x = document.getElementById("snapCanvas").style.display = "none";
+  var y = document.getElementById("takePhoto").style.display = "none";
+  var z= document.getElementById("uploadPhoto");
+     if(z.style.display==="block")
+     {
+      x.style.display = "none";
+     y.style.display = "none";
+     }
+      console.log("snap is shown");
+}
+/*function displayPicture(){
 var x = document.getElementById("takePhoto");
 if(x.style.display ==="none"){
      x.style.display = "block";
     } else {
         x.style.display ="none";
     }
+    
+   x.style.display="none";
 }*/
 
 var video = document.querySelector("#videoElement");
@@ -31,5 +47,5 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
 document.getElementById('snap').addEventListener('click',function(){
-context.drawImage(video,0,0,500,375)
+context.drawImage(video,0,0,255,255)
 });
